725f3e27651c7e29a18a11c6c00b8bdcef01a519
lazywon <jooowon.dev@gmail.com>
2022-09-12 20:01:13 +0900

    feat: make DayList and add dummy data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <DayList />
+      <Day />
+    </div>
+  );
 }
 
 export default App;
