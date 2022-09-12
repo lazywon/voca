42ef806031ab3a3eb1eda89cfcc8b6fde0125974
lazywon <jooowon.dev@gmail.com>
2022-09-12 21:59:20 +0900

    feat: useFetch custom hook 생성 및 수정, 삭제 기능 추가

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,22 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 import Word from "./Word";
 
 export default function Day() {
-  //   const day = useParams().day;
-  const { day } = useParams();
-  //   const wordList = dummy.words.filter((word) => word.day === Number(day));
-  const [words, setWords] = useState([]);
-
-  useEffect(() => {
-    fetch(`http://localhost:3002/words?day=${day}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setWords(data);
-      });
-  }, [day]);
+  const { day } = useParams(); // const day = useParams().day; 와 동일
+  const words = useFetch(`http://localhost:3002/words?day=${day}`);
 
   return (
     <table>
